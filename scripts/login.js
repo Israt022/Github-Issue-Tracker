2c8a91c422a958c6244b02e13da5c7cdbe152aec
Israt Jahan <eraisrat022@gmail.com>
2026-03-06 22:46:53 +0600

    Login Form Build completed

--- scripts/login.js
+++ scripts/login.js
@@ -1,1 +1,15 @@
-console.log("Login Page");
+const btn = document.getElementById("btn");
+
+btn.addEventListener('click',()=>{
+    const inpUserName = document.getElementById("inpName");
+    const inpPassword = document.getElementById("inpPass");
+
+    const username = inpUserName.value;
+    const password = inpPassword.value;
+
+    if(username === "admin" && password === "admin123"){
+        window.location.assign("./main.html");
+    }else{
+        window.location.assign("/")
+    }
+})
